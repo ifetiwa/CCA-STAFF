b8b192aa7915f8a16ba1990c42cf9e0be3d0960d
Tiwa Elegbeleye <p23labsdev@gmail.com>
2026-05-18 13:56:09 +0100

    ggg

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import './styles/layout.css'
 import './styles/guide.css'
+import './styles/mobile.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
